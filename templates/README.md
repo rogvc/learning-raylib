6c52d23701f8ac0a5be6fae47b181d319633eef1
Rogerio Cruz <rogvc@outlook.com>
2022-09-06 21:14:31 -0600

    Adds basic template

---
+++ templates/README.md
@@ -0,0 +1,19 @@
+# Raylib-cs Template
+
+Use this template if you want to create a new raylib project using c# and dotnet.
+
+To install the template:
+
+```pwsh
+# from project root
+cd template
+dotnet new --install ./
+```
+
+To create a new project from the template:
+
+```pwsh
+dotnet new raylibstarter
+```
+
+In the future, I plan on improving this template, turning this into a template package, and then uploading it to NuGet .
---
+++ templates/basic-template/Program.cs
@@ -0,0 +1,25 @@
+﻿using static Raylib_cs.Raylib;
+using static Raylib_cs.Color;
+
+//Initialization
+const int screenWidth = 800;
+const int screenHeight = 450;
+
+InitWindow(screenWidth, screenHeight, "Hello, raylib-cs!");
+SetTargetFPS(60); // Set target FPS (maximum)
+
+while (!WindowShouldClose()) // Detect window close button or ESC key
+{
+    // Update
+
+    // Draw
+    BeginDrawing();
+
+    ClearBackground(RAYWHITE);
+    DrawText("Congrats! You're good to go!", 250, 200, 20, DARKGRAY);
+
+    EndDrawing();
+}
+
+// De-initialization
+CloseWindow();        // Close window and OpenGL context
